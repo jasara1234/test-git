12e0fc580fb1bc10636a976076a874463c956181
jasara123 <just.jasara@gmail.com>
2025-12-04 11:30:51 +0530

    Upload my work 1

---
+++ tp-insta/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+</head>
+<body>
+    <form action="menu.php" method="get">
+        <button type="button">Commencer</button>
+    </form>
+</body>
+</html>
